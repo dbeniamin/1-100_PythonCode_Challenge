4639e42be478fbb5eed2def74a2e0fbce7a8bb10
Drimus Beniamin <104926843+dbeniamin@users.noreply.github.com>
2024-02-07 10:52:31 +0200

    Add files via upload

---
+++ day 17 - Quiz Game OOP/data.py
@@ -0,0 +1,40 @@
+question_data = [{"type": "boolean",
+                  "difficulty": "easy",
+                  "category": "History",
+                  "question": "The United States of America declared their "
+                              "independence from the British Empire on July 4th, 1776.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"type": "boolean", "difficulty": "hard", "category": "History",
+                  "question": "The man that shot Alexander Hamilton was named Aaron "
+                              "Burr.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"type": "boolean", "difficulty": "easy", "category": "History",
+                  "question": "Thomas Crapper was a plumber who invented the flushing "
+                              "toilet.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"type": "boolean", "difficulty": "easy", "category": "History",
+                  "question": "The French Kingdom helped the United States gain their "
+                              "independence over Great Britain during the "
+                              "Revolutionary War.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"type": "boolean", "difficulty": "easy", "category": "History",
+                  "question": "Adolf Hitler was a german soldier in World War I.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"type": "boolean", "difficulty": "medium", "category": "History",
+                  "question": "Brezhnev was the 5th leader of the USSR.",
+                  "correct_answer": "True", "incorrect_answers": ["False"]},
+                 {"type": "boolean", "difficulty": "hard", "category": "History",
+                  "question": "The Kingdom of Prussia briefly held land in Estonia.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"type": "boolean", "difficulty": "medium", "category": "History",
+                  "question": "The first televised presidential debate was between "
+                              "Jimmy Carter and Gerald Ford.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"type": "boolean", "difficulty": "easy", "category": "History",
+                  "question": "United States President John F. Kennedy was "
+                              "assassinated during his presidential motorcade in "
+                              "Atlanta, Georgia on November 22nd, 1963.",
+                  "correct_answer": "False", "incorrect_answers": ["True"]},
+                 {"type": "boolean", "difficulty": "hard", "category": "History",
+                  "question": "The Battle of Trafalgar took place on October 23rd, 1805",
+                  "correct_answer": "False", "incorrect_answers": ["True"]}]
---
+++ day 17 - Quiz Game OOP/question_model.py
@@ -0,0 +1,6 @@
+class Question:
+
+    def __init__(self, question_text, question_answer):
+        self.text = question_text
+        self.answer = question_answer
+
