ab92c28b2a2bc2a743e02872b3fe144fbedeeda8
Drimus Beniamin <104926843+dbeniamin@users.noreply.github.com>
2024-04-03 20:41:30 +0300

    Add files via upload

---
+++ day 33_2 - ISS overhead notifier/main.py
@@ -0,0 +1,53 @@
+import requests
+from datetime import datetime
+import smtplib
+import time
+
+MY_EMAIL = "coding_class_address@gmail.com"
+MY_PASSWORD = "coding_class_pass1234"
+MY_LAT = 44.426765  # Your latitude
+MY_LONG = 26.102537  # Your longitude
+
+
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    # Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
+
+
+while True:
+    # add a time.sleep to have a loop that is executed every 60 seconds
+    time.sleep(60)
+    if is_iss_overhead() and is_night():
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=MY_EMAIL,
+                            msg="Subject: Look up ! \n\n The ISS is up in the sky."
+                            )
