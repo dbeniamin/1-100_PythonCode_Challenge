8d33717a526dadc4a160039cd1cf9de587e76aae
Drimus Beniamin <104926843+dbeniamin@users.noreply.github.com>
2024-07-02 08:36:54 +0300

    Add files via upload

---
+++ day_49.py
@@ -0,0 +1,9 @@
+# ## LinkedIn auto applicator to jobs ###
+
+
+# skipping this day due to the changes in the platform that make auto application very hard
+# it is not impossible tho
+
+# basically the bot needs to be very complex in order to pass the questioner jobs and successfully apply with good
+# answers
+
