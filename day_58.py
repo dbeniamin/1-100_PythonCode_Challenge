bacadd3f2a0326ec11cfce03a746a34e329b1dae
Drimus Beniamin <drimus.beni@gmail.com>
2024-09-02 18:51:35 +0300

    Add files via upload

---
+++ day_58.py
@@ -0,0 +1,11 @@
+# ## bootstrap - front end toolkit - allows to add pre-made elements
+
+"""" 
+Documentation links !!
+
+https://getbootstrap.com/
+https://getbootstrap.com/docs/5.3/getting-started/introduction/
+
+https://appbrewery.github.io/bootstrap-layout/  - exercise for the bootstrap 12 column layout
+
+"""""
