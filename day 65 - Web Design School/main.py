8fd3795362d33018f92f59972e4e39771c595138
Drimus Beniamin <drimus.beni@gmail.com>
2024-11-13 14:52:03 +0200

    Add files via upload

---
+++ day 65 - Web Design School/main.py
@@ -0,0 +1,15 @@
+# ------ >> 4 pillars of web design << ------
+# 1 - Colour Theory
+# 2 - Typography
+# 3 - User Interface Design
+# 4 - User Experience Design
+
+
+# 1 - Colour Theory - colorhunt and adobe color wheel can be used as resources
+
+# 2 - Typography - combine diffrent fonts
+# serif familly - serious , older stuff
+# san serif familly -  modern
+
+# 3 - User Interface Design
+ 
